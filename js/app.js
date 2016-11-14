793b2a8aa4584432359447fb6487d05dabe3e2be
gowji <gowthamrgo@gmail.com>
2016-11-14 20:25:48 +0530

    added the attributes for enemy and created player class without filling in
    no attributes for player has been declared

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x=x;
+    this.y=y;
+    this.speed=speed;
+
 };
 
 // Update the enemy's position, required method for game
@@ -25,10 +29,12 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-
+var enemy0= new Enemy(20,100);
+var enemy1=new Enemy(40,50);
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies= [ ];
 
 
 
